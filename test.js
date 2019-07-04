a20e3ce5ae438689bc112fa880db343982b3a9f7
FrankCCCCC <unaxultraspaceos5@gmail.com>
2019-07-05 00:56:19 +0700

    Fix second and add Demo Capture

---
+++ test.js
@@ -0,0 +1,6 @@
+// import { jsonInterfaceMethodToString } from "web3-utils";
+
+// const SHA256 = require("crypto-js/sha256");
+import CryptoJS from "crypto-js";
+
+console.log(CryptoJS.sha256("III"));
